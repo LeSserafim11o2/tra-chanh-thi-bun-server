69c910c38695e4515000b3bf7a43ba0547a48dde
Le Sserafim <namnguyen.nnnn54@gmail.com>
2025-06-20 18:08:49 +0700

    fix cors error

--- index.js
+++ index.js
@@ -17,7 +17,23 @@ import { initSocketServer } from "./socket.js";
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5176",
+  "https://tra-chanh-thi-bun.vercel.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
